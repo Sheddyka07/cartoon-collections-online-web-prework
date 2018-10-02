6abf789ee8b46ef9faf1180e465d8afdfff56517
Kelly Sheddy <jungaka07@gmail.com>
2018-10-02 12:54:29 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -19,7 +19,7 @@ def long_planeteer_calls(calls)
 end
 
 
-def find_the_cheese(cheese_types)
+def find_the_cheese(cheese)
   cheese_types = ["cheddar", "gouda", "camembert"]
   cheese.find do |cheese| 
     cheese_types.include?(cheese) 
