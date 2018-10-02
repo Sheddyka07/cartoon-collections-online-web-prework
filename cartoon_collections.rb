7911369a8285c53561fc9507685152f6e20864e3
Kelly Sheddy <jungaka07@gmail.com>
2018-10-02 12:15:13 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,7 +1,7 @@
-dwarves = ["Doc", "Dopey", "Bashful", "Grumpy"]
+dwarfs = ["Doc", "Dopey", "Bashful", "Grumpy"]
 
 def roll_call_dwarves(dwarfs)
-   dwarves.each_with_index do | name , index |
+   dwarfs.each_with_index do | name , index |
     puts "#{index + 1} #{name}"
   end
 end
