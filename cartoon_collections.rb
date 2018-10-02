74783a666944513e11c13d25c3ebb8928a5c042d
Kelly Sheddy <jungaka07@gmail.com>
2018-10-02 12:13:22 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,5 +1,10 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+dwarves = ["Doc", "Dopey", "Bashful", "Grumpy"]
+
+def roll_call_dwarves(dwarves)
+  dwarves.each_with_index { |index, item|
+  hash[item] = index
+}
+
 end
 
 def summon_captain_planet# code an argument here
