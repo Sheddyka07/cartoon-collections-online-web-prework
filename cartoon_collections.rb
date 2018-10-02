2cffaebf15e8c42a763197c6eb36ecb8407482b9
Kelly Sheddy <jungaka07@gmail.com>
2018-10-02 12:45:02 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,12 +1,9 @@
-dwarfs = ["Doc", "Dopey", "Bashful", "Grumpy"]
 
 def roll_call_dwarves(dwarfs)
    dwarfs.each_with_index do | name , index |
     puts "#{index + 1} #{name}"
   end
 end
-
-
 
 def summon_captain_planet(veggies)
   veggies.collect do |fruits|
