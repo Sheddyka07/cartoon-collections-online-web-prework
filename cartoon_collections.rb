22e2e68c25f889f0613e0dd02420cc3b0343273b
Kelly Sheddy <jungaka07@gmail.com>
2018-10-02 12:54:21 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -22,5 +22,5 @@ end
 def find_the_cheese(cheese_types)
   cheese_types = ["cheddar", "gouda", "camembert"]
   cheese.find do |cheese| 
-  cheese_types.include?(cheese) 
+    cheese_types.include?(cheese) 
 end
